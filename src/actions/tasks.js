f16d45ba1d14c1e1291daa2917748bde4c4b0b06
Keishiro Nishiyama <kn11308@gmail.com>
2019-12-24 19:37:01 +0900

    initial commit

---
+++ src/actions/tasks.js
@@ -0,0 +1,13 @@
+export const addTask = (task) => ({
+  type: 'ADD_TASK',
+  task
+});
+
+// const resetTask = () => ({
+//   type: 'RESET_TASK'
+// });
+
+export const inputTask = (task) => ({
+  type: 'INPUT_TASK',
+  task
+});
--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
+import { createStore } from 'redux';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {render} from 'react-dom';
+import tasksReducer from './reducers/tasks';
+import TodoApp from './components/TodoApp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+function renderApp(store){
+  render(
+    <TodoApp store={store} />,
+    document.getElementById('root')
+  );
+}
+
+
+
+
+
+
+const store = createStore(tasksReducer);
+
+// function handleChange(){
+//   console.log(store.getState());
+// }
+// const unsbscribe = store.subscribe(handleChange);
+
+// unsbscribe()
+
+
+
+store.subscribe(() => renderApp(store));
+
+renderApp(store);
