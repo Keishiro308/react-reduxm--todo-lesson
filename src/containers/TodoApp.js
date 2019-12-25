4f49e2b9c031d93fa92efb29af4f7e436b47b82d
Keishiro Nishiyama <kn11308@gmail.com>
2019-12-25 16:49:33 +0900

    react redux

---
+++ src/containers/TodoApp.js
@@ -0,0 +1,23 @@
+import {connect} from 'react-redux';
+import {addTask, inputTask} from '../actions/tasks';
+import TodoApp from '../components/TodoApp';
+
+function mapStateToProps({task, tasks}){
+  return {
+    task,
+    tasks
+  };
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+    addTask(task){
+      dispatch(addTask(task));
+    },
+    inputTask(task){
+      dispatch(inputTask(task));
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoApp);
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import { createStore } from 'redux';
 import React from 'react';
 import {render} from 'react-dom';
 import tasksReducer from './reducers/tasks';
-import TodoApp from './components/TodoApp';
+import TodoApp from './containers/TodoApp';
+import {Provider} from 'react-redux';
 
 
 
 function renderApp(store){
   render(
-    <TodoApp store={store} />,
+    <Provider store={store} >
+      <TodoApp />
+    </Provider>,
     document.getElementById('root')
   );
 }
